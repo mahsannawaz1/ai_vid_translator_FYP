81415d752cb27e7ff0e009272cbbb386520db920
Muhammad Ahsan <nawazehsen@gmail.com>
2025-05-18 19:09:52 +0500

    change the Video model

--- backend/src/models/Video.js
+++ backend/src/models/Video.js
@@ -6,6 +6,23 @@ const videoSchema = new mongoose.Schema({
     uploadedAt: {
         type: Date,
         default: Date.now
+    },
+    outputFileName: {
+        type:String,
+        default: null
+    },
+    outputFilePath: {
+        type:String,
+        default: null
+    },
+    outputFielUploadedAt: {
+        type: Date,
+        default: null
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     }
 });
 
